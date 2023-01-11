155169ef666a17d2fc33bac815199fa5e03c01a9
RahatMinhaj <114273506+RahatMinhaj@users.noreply.github.com>
2023-01-11 11:44:57 +0600

    "bootstrap and jquery added"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'HMS-ANGULAR-SPRING-PROJECT';
+
+
+  ngOnInit(): void {
+    $("p").text("minhaj");
+   
+  }
+
+
+
+
+
 }
